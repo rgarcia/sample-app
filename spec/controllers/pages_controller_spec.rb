98ab4a1ae209fdbc761e336fc0a9a0bc08e24c32
Rafael Garcia <rgarcia2009@gmail.com>
2011-01-26 21:16:07 -0600

    better static pages

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -8,6 +8,12 @@ describe PagesController do
       get 'home'
       response.should be_success
     end
+
+    it "should have the right title" do
+      get 'home'
+      response.should have_selector("title",
+                        :content => "RoR Tutorial Sample App | Home")
+    end
   end
 
   describe "GET 'contact'" do
@@ -15,12 +21,24 @@ describe PagesController do
       get 'contact'
       response.should be_success
     end
+
+    it "should have the right title" do
+      get 'contact'
+      response.should have_selector("title",
+                        :content => "RoR Tutorial Sample App | Contact")
+    end
   end
 
   describe "GET 'about'" do
     it "should be successful" do
       get 'about'
       response.should be_success
+    end
+
+    it "should have the right title" do
+      get 'about'
+      response.should have_selector("title",
+                        :content => "RoR Tutorial Sample App | About")
     end
   end
 

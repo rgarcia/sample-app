17a441cce192a925ec20135ac124edf9711008ef
Rafael Garcia <rgarcia2009@gmail.com>
2011-02-05 16:56:26 -0600

    putting the stylesheets to use

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+
+  # Return a title on a per-page basis
+  # title method will be available in all of our views
+  def title
+    base_title = "RoR Tutorial Sample App"
+    if @title.nil?
+      base_title
+    else
+      "#{base_title} | #{@title}"
+    end
+  end
+
 end

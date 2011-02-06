f7e41a022dc973327806a26e457a2a596bbaf1b3
Rafael Garcia <rgarcia2009@gmail.com>
2011-02-06 10:36:01 -0600

    changing routes to remove pages.. prefix

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,10 @@
 SampleApp::Application.routes.draw do
-  get "pages/home"
-
-  get "pages/contact"
-
-  get "pages/about"
-
-  get "pages/help"
+  # note: these implicitly create {root,contact,about,help}_{path,url}
+  root :to => "pages#home"
+  match '/contact', :to => 'pages#contact'
+  match '/about', :to => 'pages#about'
+  match '/help', :to => 'pages#help'
+  
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
---
+++ spec/requests/layout_links_spec.rb
@@ -0,0 +1,25 @@
+require 'spec_helper'
+
+describe "LayoutLinks" do
+
+  it "should have a home page at '/'" do
+    get '/'
+    response.should have_selector('title', :content => "Home")
+  end
+
+  it "should have a contact page at '/contact'" do
+    get '/contact'
+    response.should have_selector('title', :content => "Contact")
+  end
+
+  it "should have an about page at '/about'" do
+    get '/about'
+    response.should have_selector('title', :content => "About")
+  end
+
+  it "should have a help page at '/help'" do
+    get '/help'
+    response.should have_selector('title', :content => "Help")
+  end
+  
+end

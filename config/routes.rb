21d434c6b09ee365125dceca319753c59ef9a004
Rafael Garcia <rgarcia2009@gmail.com>
2011-02-06 12:07:16 -0600

    added Users controller for signup page

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,14 @@
 SampleApp::Application.routes.draw do
+  get "users/new"
+
   # note: these implicitly create {root,contact,about,help}_{path,url}
   root :to => "pages#home"
   match '/contact', :to => 'pages#contact'
   match '/about', :to => 'pages#about'
   match '/help', :to => 'pages#help'
-  
+ 
+  match '/signup', :to => 'users#new'
+ 
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
